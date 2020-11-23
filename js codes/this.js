3dfd7727e741831eabf51c9a50d4d5bd1aa7ec77
Arif <arif.asan.jmi@gmail.com>
2020-11-23 11:42:30 +0530

    this keyord basic idea

---
+++ js codes/this.js
@@ -0,0 +1,6 @@
+// Part 1 for this keyword
+// this keyword gives the access to global context, it's vary according to situation
+// this gives access to global context in browser and in case of node it gives us an empty scope.
+
+
+console.log(this);
